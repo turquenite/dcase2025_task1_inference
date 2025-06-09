8b69b66bcdc66f891868c4701363664f0830b86b
turquenite <dominik.karasin@outlook.de>
2025-06-09 15:55:14 +0200

    Rename and adapt package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='Schmid_CPJKU_task1',
+    name='MALACH_CPJKU_task1',
     version='0.1.0',
     description='Baseline Inference package',
     author='Florian Schmid',
@@ -14,7 +14,7 @@ setup(
     install_requires=requirements,
     include_package_data=True,
     package_data={
-        'Schmid_CPJKU_task1': ["resources/*.wav", 'ckpts/*.ckpt'],
+        'MALACH_CPJKU_task1': ["resources/*.wav", 'ckpts/*.ckpt'],
     },
     python_requires='>=3.13',
 )
